4d538e6c6859db568410743f07b033d0155adcc6
Shubham Puri <shubham.puri@perficient.com>
2024-06-17 15:25:58 +0530

    Saved the files

---
+++ Java_Selenium/src/Main/Test.java
@@ -0,0 +1,20 @@
+package Main;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.co.in/");
+		driver.getTitle();
+		driver.quit();
+		
+
+	}
+
+}
