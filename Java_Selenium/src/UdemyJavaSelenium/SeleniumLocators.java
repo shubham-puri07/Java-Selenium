872209cdcd1d74ba0e18cedc1c07c774d8d1b3a3
shubham-puri07 <shubhampuri120@gmail.com>
2024-10-23 22:43:48 +0530

    added new code from learning

--- Java_Selenium/src/UdemyJavaSelenium/SeleniumLocators.java
+++ Java_Selenium/src/UdemyJavaSelenium/SeleniumLocators.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class SeleniumLocators extends Utility{
 
     public static void main(String[] args) throws InterruptedException {
